b9a9d1c5a3357a65a085eb6d0482bacf0a327c45
AlejandroMolero004 <amoleroo@alumnos.nebrija.es>
2024-09-24 16:37:31 +0200

    funciones de array for each y map

---
+++ videos.ts/arrayforeach.ts
@@ -0,0 +1,39 @@
+
+const arr=[1,2,3,4,4]
+/*
+// recibe un solo parametro que asu vez va a ser otra funcion
+const muestra=(a:number)=>console.log(a)
+
+// recorre ca uno de los elemtos del array y ejecuta la funcion que pasamos en el parameto con cada uno de los 
+// elementos
+arr.forEach(muestra)
+
+arr.forEach((a:number)=>console.log(a))
+// a ya sabe que tiene que ser un numero  porque sabe que esta
+//en un array de numeros
+arr.forEach((a)=>console.log(a))
+
+// al for each se le pueden pasar dos parametros uno que sea el 
+// elemento del array y otro el indice 
+arr.forEach((elem:number,i:number)=>{
+    console.log(elem+" "+i)
+})
+const f=(elem:number,i:number)=>{console.log(elem+" "+i)}
+arr.forEach(f)
+
+*/
+
+//EJ MOSTRAR DE UN ARRAY SOLO LOS NUMERO DE INDICE PAR 
+/*arr.forEach((elem:number,i:number)=>{
+     if(i%2==0){
+        console.log(elem+" "+"indice: "+i) 
+     } 
+})*/
+/*
+const f=(elem:number,i:number)=>{
+    if(i%2==0){
+        console.log(elem+" "+"indice: "+i) 
+     } 
+}
+arr.forEach(f)
+*/
---
+++ videos.ts/arraymap.ts
@@ -0,0 +1,34 @@
+
+//map trasformar un array en otro puede recibir dos parametros el elemto y su indice
+//pero el array de origen y el array final tienen que tener el mismo tamaño 
+
+/*
+const ARRAY=[1,2,3,4,5]
+// la funcion map recibe una funcion t la palica sobrea cada elemento
+const f=(n:number)=>2*n
+const eldoble=ARRAY.map(f)
+ARRAY.forEach((elem:number)=>{
+    console.log(elem)
+})
+console.log(" ")
+eldoble.forEach((elem:number)=>{
+    console.log(elem)
+})
+//const eldoble=ARRAY.map((n:number)=>2*n)
+
+*/
+
+type Persona={
+    name:string
+    edad:number
+}
+const Personas:Persona[]=[
+    {name:"Alberto",edad:12},
+    {name:"Alex",edad:15},
+    {name:"Aan",edad:20}
+]
+//lo que haces aqui es que del array de persona vas a coger de las persona solo su nombre
+const nombres=Personas.map((p)=>p.name)
+nombres.forEach((n:string)=>{
+    console.log(n)
+})
