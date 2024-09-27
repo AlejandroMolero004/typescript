035a96742e933cc5ab2b3b4f3a0c349809e4c616
AlejandroMolero004 <amoleroo@alumnos.nebrija.es>
2024-09-27 22:07:20 +0200

    funcion reduce de array

---
+++ apuntes/array.ts
@@ -0,0 +1,30 @@
+const numbers = [1, 222, 3, 4,22,0,1,111];
+const suma=numbers.reduce((acumulador,elemento)=>acumulador+elemento,0)
+const multiplicacion=numbers.reduce((acumulador,elemento)=>acumulador*elemento,1)
+//console.log(suma)
+//console.log(multiplicacion)
+
+const words=["Hola ","me ","llamo ","Alex "]
+const frase=words.reduce((acumulador,elemento)=>acumulador.concat(elemento),"")
+//console.log(frase)
+
+let mayor=(n1:number,n2:number)=>{
+    let mayor=0
+    if(n1>n2){mayor=n1}
+    else{
+        mayor=n2
+    }
+    return mayor
+}
+const maximo=numbers.reduce((acumulador,elemento)=>mayor(elemento,acumulador),0)
+//console.log(maximo)
+
+// Dado un array de valores, cuenta cuántas veces aparece cada valor en el array.
+/*
+const fruta=['manzana', 'plátano', 'naranja', 'manzana', 'naranja', 'plátano', 'manzana']
+
+const count=fruta.reduce((acumulador,fruta)=>{
+    
+},{})
+console.log(count)
+*/
